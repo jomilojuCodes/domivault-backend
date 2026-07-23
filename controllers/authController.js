04d906924a1727d2b441f2c74726e19042aa4dda
jomilojuCodes <adebayojomiloju958@gmail.com>
2026-07-23 20:17:28 +0100

    Block suspended users from login, fix reject landlord

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,13 +13,11 @@ const registerUser = async (req, res) => {
   try {
     const { name, email, password, role, phone } = req.body;
 
-    // Check if user exists
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Create user
     const user = await User.create({ name, email, password, role, phone });
 
     if (user) {
@@ -45,10 +43,18 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Find user
     const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Check if suspended
+    if (user.isSuspended) {
+      return res.status(403).json({ message: 'Your account has been suspended. Please contact support at support@domivault.ng' });
+    }
+
+    if (await user.matchPassword(password)) {
       res.json({
         _id: user._id,
         name: user.name,
